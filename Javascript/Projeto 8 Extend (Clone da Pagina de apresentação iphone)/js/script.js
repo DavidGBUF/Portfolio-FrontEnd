a5b48643d2c38354dda9681b25d3c44aace5afa2
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2024-03-20 10:33:11 -0300

    update

--- Javascript/Projeto 8 Extend (Clone da Pagina de apresentação iphone)/js/script.js
+++ Javascript/Projeto 8 Extend (Clone da Pagina de apresentação iphone)/js/script.js
@@ -1,9 +1,9 @@
 const buttons = document.querySelectorAll("#image-picker li")
 const image = document.querySelector("#product-image")
 
+
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
-        
         buttons.forEach((btn) => {
             btn.querySelector(".color").classList.remove("selected")
         })
@@ -14,10 +14,11 @@ buttons.forEach((btn) => {
         button.querySelector(".color").classList.add("selected")
 
         image.classList.add("changing")
-        image.setAttribute("src",`img/iphone_${id}.jpg`)
+        image.setAttribute("src", `img/iPhone 15_${id}.jpg`)
 
         setTimeout(()=>{
             image.classList.toggle("changing")
-        },200)
+        }
+        ,300)
     })
 })
